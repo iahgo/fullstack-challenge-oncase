a96f8b9b71cea572f49bc0845f885c9ac17ccbfa
iahgo <iahgo@icloud.com>
2023-02-17 16:48:59 -0300

    add foto readme

--- app/backend/src/app.js
+++ app/backend/src/app.js
@@ -9,12 +9,12 @@ const app = express();
 
 const limiter = rateLimit({
   windowMs: 60 * 1000,
-  max: 100, // limit each IP to 100 requests per windowMs
+  max: 50, // limit each IP to 50 requests per windowMs
   message: 'Too many accounts created from this IP, please try again after a minute',
 });
 
 // API SECURITY
-app.use(morgan('tiny')); // just for logs
+app.use(morgan('tiny'));
 app.use(helmet());
 app.use(cors());
 app.use(limiter);
